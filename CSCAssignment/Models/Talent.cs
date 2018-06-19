4ff572fca52422a1455a271cbd2bdda2b87cc534
francisyzy <francisyzy@IP-C61360D6>
2018-06-19 19:28:27 +0800

    Add Data Annotations

--- CSCAssignment/Models/Talent.cs
+++ CSCAssignment/Models/Talent.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -8,9 +9,13 @@ namespace CSCAssignment.Models
     public class Talent
     {
         public int Id { get; set; }
+        [Required]
         public string Name { get; set; }
+        [Required]
         public string ShortName { get; set; }
+        [Required]
         public string Reknown { get; set; }
+        [Required]
         public string Bio { get; set; }
 
     }
