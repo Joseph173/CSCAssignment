0437c5bcafb3ae724f0ce942812e9e005253695c
francisyzy <francisyzy@IP-C61360D6>
2018-06-19 17:53:58 +0800

    a

--- CSCAssignment/App_Start/WebApiConfig.cs
+++ CSCAssignment/App_Start/WebApiConfig.cs
@@ -16,6 +16,7 @@ namespace CSCAssignment
             // Configure Web API to use only bearer token authentication.
             config.SuppressDefaultHostAuthentication();
             config.Filters.Add(new HostAuthenticationFilter(OAuthDefaults.AuthenticationType));
+            config.Filters.Add(new CSCAssignment.Filters.RequireHttpsAttribute());
 
             // Web API routes
             config.MapHttpAttributeRoutes();
