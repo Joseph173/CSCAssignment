80b7a1673c918b83467b3b42ea534d685ffb2b74
francisyzy <francisyzy@IP-C61360D6>
2018-06-19 19:01:35 +0800

    Finish Task 3

--- CSCAssignment/TalentSearch/script.js
+++ CSCAssignment/TalentSearch/script.js
@@ -1,10 +1,10 @@
 $('#search').keyup(function () {
     //get data from json file
-    var urlForJson = "data.json";
+    //var urlForJson = "data.json";
 
 
     //get data from Restful web Service in development environment
-    //var urlForJson = "http://localhost:9000/api/talents";
+    var urlForJson = "http://localhost:55905/api/talents";
 
     //get data from Restful web Service in production environment
     //var urlForJson= "http://csc123.azurewebsites.net/api/talents";
