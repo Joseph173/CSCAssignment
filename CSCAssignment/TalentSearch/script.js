18bd3c1a437c0e786e8683db7b7340ca085be1e8
francisyzy <francisyzy@IP-C61360D6>
2018-06-23 16:00:12 +0800

    Add retry button

--- CSCAssignment/TalentSearch/script.js
+++ CSCAssignment/TalentSearch/script.js
@@ -1,3 +1,29 @@
+// Get the modal
+var modal = document.getElementById('myModal');
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal 
+btn.onclick = function () {
+    window.location.reload();
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function () {
+    modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function (event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+
 $('#search').keyup(function () {
     //get data from json file
     //var urlForJson = "data.json";
@@ -34,5 +60,7 @@ $('#search').keyup(function () {
         });
         output += '</ul>';
         $('#update').html(output);
+    }).fail(function (data, textStatus, jqXHR) {
+        document.getElementById('myModal').style.display = "Block";
     }); //get JSON
 });
